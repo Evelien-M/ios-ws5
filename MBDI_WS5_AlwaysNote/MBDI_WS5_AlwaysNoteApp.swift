7b540900102773b4bc2ccd14177a86f25d34da4e
Evelien-M <32199993+Evelien-M@users.noreply.github.com>
2023-02-10 11:01:32 +0100

    init

---
+++ MBDI_WS5_AlwaysNote/MBDI_WS5_AlwaysNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  MBDI_WS5_AlwaysNoteApp.swift
+//  MBDI_WS5_AlwaysNote
+//
+//  Created by Evelien Manshanden on 10/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct MBDI_WS5_AlwaysNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
