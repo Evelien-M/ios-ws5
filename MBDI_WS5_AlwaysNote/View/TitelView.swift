46d6a126822c5c6d363b5c08b2361d959495a4d0
Evelien-M <32199993+Evelien-M@users.noreply.github.com>
2023-02-10 11:12:27 +0100

    Update TitelView.swift

--- MBDI_WS5_AlwaysNote/View/TitelView.swift
+++ MBDI_WS5_AlwaysNote/View/TitelView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct TitelView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("AlwaysNote").font(.custom("Hoefler Text", size: 60)).foregroundColor(.yellow)
     }
 }
 
